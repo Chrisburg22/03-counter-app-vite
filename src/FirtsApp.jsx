760f13c3811fda24a6ad163a33ce09fe1e170dec
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2022-11-09 12:58:45 -0600

    Funtions in JSx

--- src/FirtsApp.jsx
+++ src/FirtsApp.jsx
@@ -1,22 +1,15 @@
 
-
-const newMessage = {
-    message: 'Hola Mundo',
-    title: 'Christian'
+const getResult = (a,b) => {
+    return a + b;
 }
 
 export const FirstApp = () =>{
 
-    const funcionEjemplo = () => {
-        const palabraNueva = newMessage.title + " Ramos Peres y así podria crear una función más com pleja";
-        return palabraNueva;
-    };
-
     return ( 
         <>
-            <h1>{funcionEjemplo()}</h1> 
+            <h1>{getResult(10,11)}</h1> 
             {/*<code> { JSON.stringify{ newMessage } } </code> */}
             <p>Curso de React</p>
-        </>//Esto es lo mismo que el Fragment
+        </>
     );
 }
