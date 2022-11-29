e176179f871e94f386f88aa176a073cc2ec610ab
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2022-11-29 08:36:34 -0600

    Manejando Hooks

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,30 +1,25 @@
 import { useState } from 'react'
 import Proptypes from 'prop-types'
 
-//HOOK
+
 export const CounterApp = ({ value }) => {
-    
+    //HOOK 
     const [counter, setCounter] = useState(value);
     
     const handleAdd = () => {
-        /**
-         * event nos servira para conocer imformacion del evento
-         * console.log(event);
-         * 
-         * Esta es una alternativa para cambiar el estado 
-         * setCounter( (c) => c+1 );
-         */
         setCounter( counter+1 );
     }
+    const handleSubstract = () => setCounter( counter-1 );
+    const handleReset = () => setCounter(value);
 
     return (
     <>
       <h1>CounterApp</h1>
       <h2>{ counter }</h2>
 
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={ handleAdd }>+1</button>
+      <button onClick={ handleSubstract }>-1</button>
+      <button onClick={ handleReset }>Reset</button>
     </>
   )
 }
